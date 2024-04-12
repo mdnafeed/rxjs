6894fc064e303a3e82b5e855b60e18655bfc2d4f
Md Nafeed Razy Akram <nafeedjmi@gmail.com>
2024-04-13 01:35:56 +0530

    Working on Obserbale and promise

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Markdown syntax guide
+# Promise & Observable
+
+## Promise
+
+* It is eager loading
+* promises handle one-time asynchronous tasks
+
+
+## Observable
+
+* It is lazy loading
+* observables handle continuous streams of asynchronous data
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,96 @@
 import { Observable } from 'rxjs'
 
 
-const obs = new Observable((observer)=>{
-    const url =  'https://dummyjson.com/products/1';
-    const http = new XMLHttpRequest();
+// function http(consumer:any,isPromise = false){
+
+//     const url =  'https://dummyjson.com/products/1';
+//     const http = new XMLHttpRequest();
+
+//     const onload = ()=>{
+//         if(http.status == 200 && http.readyState === 4){
+//             if(isPromise){
+//                 consumer(http.responseText)
+//             }
+//             else{
+//                 consumer.next(http.responseText)
+//             }
+//         }
+//     }
+
+//     const onError = ()=>{
+//         consumer.error("Error")
+//     }
+
+//     http.addEventListener('load', onload)
+
+//     http.addEventListener('error',onError)
+
+//     http.open('GET',url);
+//     http.send();
+    
+// }
+
+// const pr = new Promise((resolve)=>{
+//     console.log('Promise Start');
+//     http(resolve,true);
+// });
+
+// setTimeout(()=>{
+//     pr.then((data)=>{
+//         console.log(data)
+//     })
+// },4000)
+
+// const obs = new Observable((observer)=>{
+//     console.log('Obs Start')
+//     http(observer);
+// })
+
+// obs.subscribe({
+//     next: (data)=>console.log(data)
+// })
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// Using RxJx
+
+// const obs = new Observable((observer)=>{
+//     const url =  'https://dummyjson.com/products/1';
+//     const http = new XMLHttpRequest();
 
 
-    const onload = ()=>{
-        if(http.status == 200 && http.readyState === 4){
-            observer.next(http.responseText)
-        }
-    }
+//     const onload = ()=>{
+//         if(http.status == 200 && http.readyState === 4){
+//             observer.next(http.responseText)
+//         }
+//     }
 
-    const onError = ()=>{
-        observer.error("Error")
-    }
+//     const onError = ()=>{
+//         observer.error("Error")
+//     }
 
-    http.addEventListener('load', onload)
+//     http.addEventListener('load', onload)
 
-    http.addEventListener('error',onError)
+//     http.addEventListener('error',onError)
 
-    http.open('GET',url);
-    http.send();
+//     http.open('GET',url);
+//     http.send();
 
-    return ()=>{
-        http.removeEventListener('load' , onload)
-        http.removeEventListener('error', onError)
-    }
-}) 
-
-
-const resSub = obs.subscribe({
-    next : (data) => console.log(data),
-    error: (error) => console.log(error)
-})
+//     return ()=>{
+//         http.removeEventListener('load' , onload)
+//         http.removeEventListener('error', onError)
+//     }
+// }) 
 
 
+// const resSub = obs.subscribe({
+//     next : (data) => console.log(data),
+//     error: (error) => console.log(error)
+// })
 
+// ----------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Observable using Velina Javascript
 
 // const obs = new Observable((observer)=>{
 //     let counter = 0;
