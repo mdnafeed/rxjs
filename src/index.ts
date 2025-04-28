cf2ae6a58635791a556a61a740f105e226764610
Nafeed Akram <nafeedjmi@gmail.com>
2025-04-28 10:41:40 +0530

    refs >> working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { of, interval, from, timer } from "rxjs"
-import { filter, first, last, skip, skipLast, skipUntil, take, takeLast, takeUntil, takeWhile } from "rxjs/operators"
+import { of, interval, from, timer, fromEvent, Observable, Subject, Subscriber, BehaviorSubject } from "rxjs"
+import { filter, first, last, mergeMap, skip, skipLast, skipUntil, switchMap, take, takeLast, takeUntil, takeWhile } from "rxjs/operators"
 
 // interval(1000).pipe(
 //   take(5)
@@ -22,3 +22,72 @@ from(["Nafeed","Akram","Jamia","Millia","Islamia","New Delhi"]).pipe(
   // skipUntil() ??
     // skipWhile() ?? 
 ).subscribe(console.log)
+
+// const button = document.getElementById('myBtn');
+// if(button){
+//   fromEvent(button, "click").pipe(
+//     switchMap(()=> interval(1000))
+//   ).subscribe((res) => console.log(res));
+// }
+
+// function fetchUser(userId:string){
+//   return new Observable(observer =>{
+//     console.log(userId)
+//     observer.next({id: userId, name: "Name "}),
+//     observer.complete()
+//   })
+// }
+
+
+// of("Deljhi","Kolkayta","Jamia").pipe(
+//   mergeMap((userId) => fetchUser(userId))
+// ).subscribe({
+//   next: (user)=> console.log(user),
+//   complete: () => console.log("Completed")
+// })
+
+
+// const subscription1 = interval(100).subscribe((res) => console.log(res));
+// const subscription2 = interval(200).subscribe((res) => console.log(res));
+
+// subscription1.add(subscription2);
+
+// setTimeout(()=>{
+//   subscription1.unsubscribe();
+// },2000)
+
+// const subject = new Subject<number>();
+// subject.subscribe(value => console.log("First Value ", value));
+// subject.subscribe(value => console.log("Second value ",value));
+
+// // subject.next(40);
+
+// const obserable = from([1,2,3,4,5,6]);
+
+// obserable.subscribe(subject);
+
+// Whithout Multicasting
+
+// const obserable = new Observable(subscriber =>{
+//   console.log("Is Observable")
+//   subscriber.next(Math.random());
+// })
+
+// obserable.subscribe(res => console.log(res));
+// obserable.subscribe(res => console.log(res));
+
+// // With Multicasting
+
+// const subject = new Subject();
+// subject.subscribe((res) => console.log(res));
+// subject.subscribe((res) => console.log(res));
+
+// subject.next(Math.random)
+
+const behaviorSubject  = new BehaviorSubject<number>(0);
+behaviorSubject.subscribe(value => console.log("First Value ", value));
+behaviorSubject.subscribe(value => console.log("Second value ",value));
+behaviorSubject.next(40);
+behaviorSubject.next(50);
+
+behaviorSubject.subscribe(value => console.log("Third value ",value));
